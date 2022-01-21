870f16f1731a0ad4998448825ea2a50edd51c5ad
remagart <jfmamjjasond418@gmail.com>
2022-01-22 01:54:03 +0800

    basic navigate

---
+++ lib/screen/page1.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class page1 extends StatelessWidget {
+  const page1({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('page1'),
+      ),
+    );
+  }
+}
---
+++ lib/screen/page2.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class page2 extends StatelessWidget {
+  const page2({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('page2'),
+      ),
+    );
+  }
+}
