b785fa358f2ed0cd280f204a4d3e18ef535ed099
remagart <jfmamjjasond418@gmail.com>
2022-02-19 16:47:26 +0800

    [Add] 補充說明
    20220219

--- lib/screen/practice_navigation/practice_navigation_index.dart
+++ lib/screen/practice_navigation/practice_navigation_index.dart
@@ -8,19 +8,33 @@ class practice_navigation_index extends StatelessWidget {
     return Container(
         decoration: const BoxDecoration(color: Colors.white),
         alignment: Alignment.center,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
+        child: Column(
           children: [
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/page1');
-                },
-                child: const Text('navigate to page1')),
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/page2');
-                },
-                child: const Text('navigate to page2'))
+            const SizedBox(height: 64),
+            const Text(
+              "1. navigate to page1 練習不同頁面傳送資料 和 回傳",
+              style: TextStyle(fontSize: 14, color: Colors.black),
+            ),
+            const SizedBox(height: 16),
+            const Text(
+              "2. navigate to page2 練習導向到另外一頁",
+              style: TextStyle(fontSize: 14, color: Colors.black),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/page1');
+                    },
+                    child: const Text('navigate to page1')),
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/page2');
+                    },
+                    child: const Text('navigate to page2'))
+              ],
+            )
           ],
         ));
   }
