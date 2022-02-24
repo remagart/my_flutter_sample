9b1127d4c7e0e42157be7587a0ee1c243c1ddce7
remagart <jfmamjjasond418@gmail.com>
2022-02-24 21:43:44 +0800

    [Add] lifecycle practice

--- lib/screen/lifecycle/lifecycle_index.dart
+++ lib/screen/lifecycle/lifecycle_index.dart
@@ -9,15 +9,25 @@ class lifecycle_index extends StatefulWidget {
   @override
   State<lifecycle_index> createState() {
     log("create state");
+    Fluttertoast.showToast(msg: "create state");
     return _lifecycle_indexState();
   }
 }
 
 class _lifecycle_indexState extends State<lifecycle_index> {
+  int _counter = 0;
+
+  void _increaseCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
   @override
   void initState() {
     super.initState();
     log("init state");
+    Fluttertoast.showToast(msg: "init state");
   }
 
   @override
@@ -26,6 +36,7 @@ class _lifecycle_indexState extends State<lifecycle_index> {
     super.didChangeDependencies();
 
     log("didChangeDependencies");
+    Fluttertoast.showToast(msg: "didChangeDependencies");
   }
 
   @override
@@ -34,6 +45,15 @@ class _lifecycle_indexState extends State<lifecycle_index> {
     super.didUpdateWidget(oldWidget);
 
     log("didUpdateWidget");
+    Fluttertoast.showToast(msg: "didUpdateWidget");
+  }
+
+  @override
+  void reassemble() {
+    // TODO: implement reassemble
+    super.reassemble();
+    log("reassemble");
+    Fluttertoast.showToast(msg: "reassemble");
   }
 
   @override
@@ -42,6 +62,7 @@ class _lifecycle_indexState extends State<lifecycle_index> {
     super.deactivate();
 
     log("deactivate");
+    Fluttertoast.showToast(msg: "deactivate");
   }
 
   @override
@@ -50,11 +71,13 @@ class _lifecycle_indexState extends State<lifecycle_index> {
     super.dispose();
 
     log("dispose");
+    Fluttertoast.showToast(msg: "dispose");
   }
 
   @override
   Widget build(BuildContext context) {
     log("build");
+    Fluttertoast.showToast(msg: "build");
 
     return Container(
       decoration: const BoxDecoration(color: Colors.white),
@@ -72,9 +95,80 @@ class _lifecycle_indexState extends State<lifecycle_index> {
               onPressed: () {
                 Navigator.pushNamed(context, "/lifecycle_page2");
               },
-              child: const Text("到下一頁"))
+              child: const Text("到下一頁")),
+          Text("現在的數字為 $_counter "),
+          ElevatedButton(onPressed: _increaseCounter, child: const Text("+1")),
+          childrenWidget(),
         ],
       ),
     );
+  }
+}
+
+class childrenWidget extends StatefulWidget {
+  childrenWidget({Key? key}) : super(key: key);
+
+  @override
+  State<childrenWidget> createState() {
+    log("(3) createState");
+    Fluttertoast.showToast(msg: "(3) createState");
+    return _childrenWidgetState();
+  }
+}
+
+class _childrenWidgetState extends State<childrenWidget> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    log("(3) init");
+    Fluttertoast.showToast(msg: "(3) init");
+  }
+
+  @override
+  void didChangeDependencies() {
+    // TODO: implement didChangeDependencies
+    super.didChangeDependencies();
+    log("(3) didChangeDependencies");
+    Fluttertoast.showToast(msg: "(3) didChangeDependencies");
+  }
+
+  @override
+  void reassemble() {
+    // TODO: implement reassemble
+    super.reassemble();
+    log("(3) reassemble");
+    Fluttertoast.showToast(msg: "(3) reassemble");
+  }
+
+  @override
+  void didUpdateWidget(covariant childrenWidget oldWidget) {
+    // TODO: implement didUpdateWidget
+    super.didUpdateWidget(oldWidget);
+    log("(3) didUpdateWidget");
+    Fluttertoast.showToast(msg: "(3) didUpdateWidget");
+  }
+
+  @override
+  void deactivate() {
+    // TODO: implement deactivate
+    super.deactivate();
+    log("(3) deactivate");
+    Fluttertoast.showToast(msg: "(3) deactivate");
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    log("(3) dispose");
+    Fluttertoast.showToast(msg: "(3) dispose");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    log("(3) build");
+    Fluttertoast.showToast(msg: "(3) build");
+    return Text("I am Children widget");
   }
 }
--- lib/screen/lifecycle/lifecycle_page2.dart
+++ lib/screen/lifecycle/lifecycle_page2.dart
@@ -1,13 +1,15 @@
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class lifecycle_page2 extends StatefulWidget {
   lifecycle_page2({Key? key}) : super(key: key);
 
   @override
   State<lifecycle_page2> createState() {
-    log("[2]create State");
+    log("[2] create State");
+    Fluttertoast.showToast(msg: "[2] create State");
     return _lifecycle_page2State();
   }
 }
@@ -16,7 +18,8 @@ class _lifecycle_page2State extends State<lifecycle_page2> {
   @override
   void initState() {
     super.initState();
-    log("[2]init state");
+    log("[2] init state");
+    Fluttertoast.showToast(msg: "[2] init state");
   }
 
   @override
@@ -24,7 +27,24 @@ class _lifecycle_page2State extends State<lifecycle_page2> {
     // TODO: implement didChangeDependencies
     super.didChangeDependencies();
 
-    log("[2]didChangeDependencies");
+    log("[2] didChangeDependencies");
+    Fluttertoast.showToast(msg: "[2] didChangeDependencies");
+  }
+
+  @override
+  void reassemble() {
+    // TODO: implement reassemble
+    super.reassemble();
+    log("[2] reassemble");
+    Fluttertoast.showToast(msg: "[2] reassemble");
+  }
+
+  @override
+  void didUpdateWidget(covariant lifecycle_page2 oldWidget) {
+    // TODO: implement didUpdateWidget
+    super.didUpdateWidget(oldWidget);
+    log("[2] didUpdateWidget");
+    Fluttertoast.showToast(msg: "[2] didUpdateWidget");
   }
 
   @override
@@ -32,7 +52,8 @@ class _lifecycle_page2State extends State<lifecycle_page2> {
     // TODO: implement deactivate
     super.deactivate();
 
-    log("[2]deactivate");
+    log("[2] deactivate");
+    Fluttertoast.showToast(msg: "[2] deactivate");
   }
 
   @override
@@ -40,12 +61,14 @@ class _lifecycle_page2State extends State<lifecycle_page2> {
     // TODO: implement dispose
     super.dispose();
 
-    log("[2]dispose");
+    log("[2] dispose");
+    Fluttertoast.showToast(msg: "[2] dispose");
   }
 
   @override
   Widget build(BuildContext context) {
     log("[2] build");
+    Fluttertoast.showToast(msg: "[2] build");
     return Container(
       decoration: const BoxDecoration(color: Colors.white),
       padding: const EdgeInsets.only(top: 64),
