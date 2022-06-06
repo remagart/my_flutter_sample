7f383669d9123a8030d81b867630b1fff896390c
remagart <jfmamjjasond418@gmail.com>
2022-06-06 13:57:52 +0800

    stream sample

--- lib/screen/004_Stream/index.dart
+++ lib/screen/004_Stream/index.dart
@@ -1,4 +1,24 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+
+class NumberCreator {
+  var _count = 1;
+  final _controller = StreamController<int>();
+
+  NumberCreator() {
+    Timer.periodic(Duration(seconds: 1), (t) {
+      if (_count == 3) {
+        _controller.addError("this is error");
+        _count++;
+      }
+      _controller.sink.add(_count);
+      _count++;
+    });
+  }
+
+  Stream<int> get stream => _controller.stream;
+}
 
 class streamIndex extends StatelessWidget {
   String barName = "";
@@ -10,7 +30,28 @@ class streamIndex extends StatelessWidget {
       appBar: AppBar(
         title: Text("Practice of flutter"),
       ),
-      body: Text('aaa'),
+      body: Container(
+        margin: EdgeInsets.all(100),
+        width: 200,
+        height: 200,
+        child: FittedBox(
+          fit: BoxFit.contain,
+          child: StreamBuilder<String>(
+            stream: NumberCreator().stream.map((i) => 'String $i').take(5),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Text("No data yet!");
+              } else if (snapshot.connectionState == ConnectionState.done) {
+                return Text("Done!");
+              } else if (snapshot.hasError) {
+                return Text("Error!");
+              } else {
+                return Text(snapshot.data ?? "");
+              }
+            },
+          ),
+        ),
+      ),
     );
   }
 }
