e1291b4afd33c813dac5c5df57521b325aeb1fa6
remagart <jfmamjjasond418@gmail.com>
2022-06-09 16:59:14 +0800

    future build

--- lib/screen/003_Future/index.dart
+++ lib/screen/003_Future/index.dart
@@ -10,7 +10,54 @@ class futureIndex extends StatelessWidget {
       appBar: AppBar(
         title: Text(barName),
       ),
-      body: Text('aaa'),
+      body: Column(
+        children: [
+          Container(
+            margin: const EdgeInsets.only(left: 100, right: 100),
+            width: 200,
+            height: 200,
+            child: FittedBox(
+              fit: BoxFit.contain,
+              child: FutureBuilder<String>(
+                future: Future.delayed(
+                    Duration(seconds: 3), () => 'I\'m the final data'),
+                builder: (context, snapshot) {
+                  if (snapshot.hasError) {
+                    return Text("Error!");
+                  } else if (snapshot.hasData) {
+                    // See whether need json.decode
+                    return Text(snapshot.data ?? "No data");
+                  } else {
+                    return Text("Wait 3 seconds...");
+                  }
+                },
+              ),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(left: 100, right: 100),
+            width: 200,
+            height: 200,
+            child: FittedBox(
+              fit: BoxFit.contain,
+              child: FutureBuilder<String>(
+                future: Future.delayed(
+                    Duration(seconds: 3), () => throw Exception()),
+                builder: (context, snapshot) {
+                  if (snapshot.hasError) {
+                    return Text("Error is occurred!");
+                  } else if (snapshot.hasData) {
+                    // See whether need json.decode
+                    return Text(snapshot.data ?? "No data");
+                  } else {
+                    return Text("Wait 3 seconds...");
+                  }
+                },
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
