db75db9e5e1085ff0c85584f3d2ec99e0079b96f
remagart <jfmamjjasond418@gmail.com>
2022-06-10 18:18:59 +0800

    isolate sample

--- lib/screen/006_isolate_event_loop/index.dart
+++ lib/screen/006_isolate_event_loop/index.dart
@@ -1,14 +1,41 @@
 import 'package:flutter/material.dart';
+import 'dart:isolate';
+import '../../isolateHelper/myisolate.dart';
 
-class isolateIndex extends StatelessWidget {
+class isolateIndex extends StatefulWidget {
   String barName = "";
   isolateIndex({Key? key, this.barName = "Practice"}) : super(key: key);
+
+  @override
+  State<isolateIndex> createState() => _isolateIndexState(barName: barName);
+}
+
+class _isolateIndexState extends State<isolateIndex> {
+  String mybarName = "222";
+  _isolateIndexState({required String barName}) {
+    mybarName = barName;
+  }
+
+  void foo(SendPort sendPort) {
+    sendPort.send("Hello lqr");
+  }
+
+  void runMyIsolate() {
+    main1();
+  }
+
+  @override
+  void reassemble() {
+    // TODO: implement reassemble
+    super.reassemble();
+    runMyIsolate();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Practice of flutter"),
+        title: Text(mybarName),
       ),
       body: Text('aaa'),
     );
